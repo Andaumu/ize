834fa1be391d08e12fcc2e23d14cf3bce4cb7513
Andaumu <123367708+Andaumu@users.noreply.github.com>
2026-05-15 06:06:43 +0700

    Delete pign.py

--- pign.py
+++
@@ -1,8 +0,0 @@
-python -c "
-with open('k2.py', 'r') as f:
-    c = f.read()
-c = c.replace('SEPA_BIN', 'SEPAY_BIN').replace('SEPA_ACCOUNT_NUMBER', 'SEPAY_ACCOUNT_NUMBER')
-with open('k2.py', 'w') as f:
-    f.write(c)
-print('Đã thay thế xong.')
-"
